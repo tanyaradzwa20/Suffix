32286befc3214df44c893e7a45a7dd31e4ba6b6d
Tanya <tanya@uncommon.org>
2022-07-13 14:46:15 +0200

    all

---
+++ main.js
@@ -0,0 +1,15 @@
+// var h2 = "red";
+
+// // document.getElementsByClassName("logo").style.width = 24
+// document.getElementsById("logo").style.color = "red";
+// ;
+
+// // var logo = document.getElementsByClassName("logo")
+// //  logo .style .background = "blue"
+
+//  var hOnes = document.querySelectorAll("h1");
+
+//  hOnes.forEach(h1=>{
+//      h1.innerText="Tawanda";
+//  })
+
